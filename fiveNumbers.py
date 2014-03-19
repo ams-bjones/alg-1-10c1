afc2c29d6addc13a2c8d113d68bd3180fe4b617a
Brian Jones <bjones@ashmanorschool.com>
2014-03-19 06:38:04 -0400

    five numbers done

--- fiveNumbers.py
+++ fiveNumbers.py
@@ -1,1 +1,8 @@
 #add the list of five numbers
+
+def total (fiveNumbers):
+    total = fiveNumbers[0]
+    fiveNumbers = fiveNumbers[1:]
+    for item in fiveNumbers:
+        total = total + item
+    return (total)
