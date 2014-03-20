2d685486c9dc72ae634e4812325b6be1ad90b212
Brian Jones <bjones@ashmanorschool.com>
2014-03-20 06:39:36 -0400

    added min max stuff

--- minMax.py
+++ minMax.py
@@ -1,1 +1,20 @@
 #question 2 in code
+def min (numList):
+    ''' Finds the minimun from a list of numbers '''
+    answer = None
+    for num in numList:
+        if answer == None:
+            answer = num
+        if num < answer:
+            answer = num
+    return answer
+
+def max (numList):
+    ''' finds the maximum from a list of numbers'''
+    answer = None
+    for num in numList:
+        if answer == None:
+            answer = num
+        if num > answer:
+            answer = num
+    return answer
--- minmaxtests.py
+++ minmaxtests.py
@@ -1,1 +1,27 @@
 #testing min and max functions
+import minMax
+import unittest
+
+class TestSequenceFunctions(unittest.TestCase):
+
+
+
+    def test1(self):
+        input1 = [10,9,8,7,6,5] 
+        assert minMax.min(input1)==5 #Test 1 fails
+        
+    def test2(self):
+        input2 = [4,5,6,7,8,9,10]
+        assert minMax.min(input2)==4 #Test 2 fails
+    
+    def test3(self):
+        input1 = [10,9,8,7,6,5] 
+        assert minMax.max(input1)==10 #Test 3 fails
+
+    def test4(self):
+        input2 = [4,5,6,7,8,9,10] 
+        assert minMax.max(input2)==10 #Test 4 fails
+
+
+if __name__ == '__main__':
+    unittest.main()
